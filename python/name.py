0b156a6bd83ed7c1d0abe8e5d84a136d3c4e6095
Reham Fattoum <rehamfatoum777@gmail.com>
2021-10-18 13:03:43 +0300

    python

---
+++ python/name.py
@@ -0,0 +1,29 @@
+# name=input ("Enter your name")
+# print("hello"+ name)
+# num=int(input("enter number:")) 
+# if num > 0:
+#     print("The Number is positive")
+# elif num < 0:
+#     print("The Number is Negative")
+# else:
+#     print("Number is Zero")    
+
+# name = input("Enter your name:")
+# print(f"Hello {name} , How are you??")
+# sister = ["reham","rita","yara","sara","rama"]
+# print(sister)
+# print(sister[2])
+# sister.sort()
+# sister.append("ali")
+# sister.sort()
+# print(sister)
+# friends ={"key01":"Bilal","key02":"Nour"}
+# sister = ["reham","rita","yara","sara","rama"]
+# for i in range(4):
+#     print(sister[i])
+# def BMI ():
+height=int(input("enter your height in cm please "))
+weight=int(input("enter your weight in cm please "))
+bmi=int()
+bmi=weight/((height/100)*(height/100))
+print(round(bmi,2))
