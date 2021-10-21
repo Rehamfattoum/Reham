f025b64e2bbf22a8b87a5c297b8ba667fe4dbc97
RehamFattoum <rehamfatoum777@gmail.com>
2021-10-21 04:19:49 +0300

    happy birthday with gui

--- python/name.py
+++ python/name.py
@@ -1,3 +1,5 @@
+from datetime import date
+from tkinter import *
 # import sys
 # from function import sum
 # from function import min
@@ -76,19 +78,45 @@
 # p = Point(8,2)
 # print(p.x)
 # print(p.y)
-class Flight ():
-    def __init__(self,capacity):
-        self.capacity=capacity
-        self.passengers=[]
+# class Flight ():
+#     def __init__(self,capacity):
+#         self.capacity=capacity
+#         self.passengers=[]
 
-    def add_passengers (self,name)
-    if not open_seat ()
-        self.passengers.append(name)
-        return true
+#     def add_passengers (self,name)
+#     if not open_seat ()
+#         self.passengers.append(name)
+#         return true
     
-    def open_seat (self)
-    self.capacity - Len(self.passengers)
-    return
+#     def open_seat (self)
+#     self.capacity - Len(self.passengers)
+#     return
 
-Flight = Flight(4):
-("reham","rita","ali","yara","sara")
+# Flight = Flight(4):
+# ("reham","rita","ali","yara","sara")
+
+main_window = Tk()
+Label(main_window,Text="year of birthday").pack().grid(row=0,column=0)
+year=Entry(main_window,width= 50 ,borderwidth=5).grid(row=0,column=1)
+Label(main_window,Text=" month of birthday").pack().grid(row=1,column=0)
+month=Entry(main_window,width= 50 ,borderwidth=5).grid(row=1,column=1)
+Label(main_window,Text="day of birthday").pack().grid(row=2,column=0)
+day=Entry(main_window,width= 50 ,borderwidth=5).grid(row=2,column=1)
+Button(main_window,text="My age" ,command=onclick).grid(row=3,column=0)
+def onclick ():
+    if age.month==month and age.day==day:
+        print(("Happy Birthday") )
+    else:
+        print((f"your age is:{age.year-year}") )
+
+main_window.mainloop() 
+
+
+# age=date.today()
+# year=int(input("enter your year of birthday"))
+# month=int(input("enter your month of birthday"))
+# day=int(input("enter your day of birthday"))
+# if age.month==month and age.day==day:
+#     print(("Happy Birthday") )
+# else:
+#     print((f"your age is:{age.year-year}") )
